3e4b040cac34315fe55fe11d98e9c5481e40b00a
coned <coned@outlook.com>
2019-07-03 18:38:59 +0800

    add five_game.html

--- test.py
+++ test.py
@@ -3,10 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return redirect('/login')
-
-@app.route('/login')
-def login():
-	return render_template('login.html')
-
-app.run(host='0.0.0.0')
+	return redirect('five_game.html')
